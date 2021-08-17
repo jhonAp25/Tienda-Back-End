c39122a5f9fab758c55228831faf518c8d2b7a63
JhonAp25 <apazalaricojhon25@gmail.com>
2021-08-16 20:29:07 -0500

    config Swagger

--- src/main/java/com/apaza/Tienda20/config/SwaggerConfig.java
+++ src/main/java/com/apaza/Tienda20/config/SwaggerConfig.java
@@ -2,14 +2,18 @@ package com.apaza.Tienda20.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
     @Bean
     public Docket api() {
@@ -19,4 +23,22 @@ public class SwaggerConfig {
                 .paths(PathSelectors.any())
                 .build();
     }
+
+    private ApiInfo info(){
+        return new ApiInfo(
+                "API MATRICULA",
+                "",
+                "1.0",
+                "https://www.apache.org/licenses/LICENSE-2.0.html",
+                new Contact(
+                        "",
+                        "",
+                        ""),
+                "Apache 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0.html",
+                Collections.emptyList()
+        );
+    }
+
+
 }
---
+++ src/main/java/com/apaza/Tienda20/config/SwaggerIndex.java
@@ -0,0 +1,13 @@
+package com.apaza.Tienda20.config;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SwaggerIndex {
+
+    @RequestMapping("/")
+    public String index() {
+        return "redirect:swagger-ui/index.html";
+    }
+}

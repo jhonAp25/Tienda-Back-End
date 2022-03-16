475279672267ba4510fa3ce3fff79697171ba88f
JhonAp25 <apazalaricojhon25@gmail.com>
2022-03-15 19:16:10 -0500

    new TiendaController(correcion de error)

--- src/main/java/com/apaza/Tienda20/repository/TipoProductoRepository.java
+++ src/main/java/com/apaza/Tienda20/repository/TipoProductoRepository.java
@@ -1,13 +1,11 @@
 package com.apaza.Tienda20.repository;
 
 import com.apaza.Tienda20.entity.TipoProducto;
-import org.springframework.data.jdbc.repository.query.Query;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 
 
 public interface TipoProductoRepository extends JpaRepository<TipoProducto , Long> {
-    List<TipoProducto> findByModeloAndNombreContains(String dato);
 
 }

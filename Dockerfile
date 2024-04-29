86cf73ec465fc9290f1d011a7eab69669e107353
JhonAp25 <apazalaricojhon25@gmail.com>
2024-04-28 19:52:52 -0500

    add target - dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM amazoncorretto:17-alpine-jdk
+
 COPY target/Tienda2.0-0.0.1-SNAPSHOT.jar app.jar
 
 ENTRYPOINT ["java", "-jar" , "/app.jar"]
---
+++ target/classes/import.sql
@@ -0,0 +1,75 @@
+insert into tienda values(null, 'Tienda01');
+insert into tienda values(null, 'Tienda02');
+
+insert into color values(null, '#F03434', 'rojo');
+insert into color values(null, '#17A589', 'verde jade');
+insert into color values(null, '#0D1035', 'azul');
+insert into color values(null, '#141414', 'negro');
+insert into color values(null, '#8F8F8F', 'plomo plata');
+insert into color values(null, '#3B3C3C', 'plomo rata');
+
+insert into talla values (null, 'S');
+insert into talla values (null, 'M');
+insert into talla values (null, 'L');
+insert into talla values (null, 'XL');
+insert into talla values (null, 'XXL');
+insert into talla values (null, 'XXXL');
+
+
+insert into tipo_producto values (null,'tipo01');
+insert into tipo_producto values (null,'tipo02');
+insert into tipo_producto values (null,'tipo03');
+
+insert into modelo values (null,'https://i.imgur.com/2HHNASm.png','modelo01');
+insert into modelo values (null,'https://i.imgur.com/P1GbjOY.png','modelo02');
+insert into modelo values (null,'https://i.imgur.com/WMbqAPq.png','modelo03');
+insert into modelo values (null,'https://i.imgur.com/pZyek7B.png','modelo04');
+insert into modelo values (null,'https://i.imgur.com/jgrpjpJ.png','modelo05');
+insert into modelo values (null,'https://i.imgur.com/SJDeNJn.png','modelo06');
+
+insert into modelo values (null,'https://i.imgur.com/R5kKJvP.png','modelo07');
+insert into modelo values (null,'https://i.imgur.com/OOjfMCM.png','modelo08');
+insert into modelo values (null,'https://i.imgur.com/3C0oTzj.png','modelo09');
+insert into modelo values (null,'https://i.imgur.com/RQ4gtMg.png','modelo10');
+insert into modelo values (null,'https://i.imgur.com/Btg1UPQ.png','modelo11');
+insert into modelo values (null,'https://i.imgur.com/MwtLTBA.png','modelo12');
+
+insert into modelo values (null,'https://i.imgur.com/87hPRYu.png','modelo13');
+insert into modelo values (null,'https://i.imgur.com/MKRG2Bl.png','modelo14');
+insert into modelo values (null,'https://i.imgur.com/WMbqAPq.png','modelo15');
+insert into modelo values (null,'https://i.imgur.com/pZyek7B.png','modelo16');
+insert into modelo values (null,'https://i.imgur.com/jgrpjpJ.png','modelo17');
+insert into modelo values (null,'https://i.imgur.com/SJDeNJn.png','modelo18');
+
+insert into tipo_producto_modelo  values ( 1 , 1 );
+insert into tipo_producto_modelo  values ( 1 , 2 );
+insert into tipo_producto_modelo  values ( 1 , 3 );
+insert into tipo_producto_modelo  values ( 1 , 4 );
+
+insert into tipo_producto_modelo  values ( 2 , 5 );
+insert into tipo_producto_modelo  values ( 2 , 6 );
+insert into tipo_producto_modelo  values ( 2 , 7 );
+insert into tipo_producto_modelo  values ( 2 , 8 );
+
+insert into tipo_producto_modelo  values ( 3 , 9 );
+insert into tipo_producto_modelo  values ( 3 , 10 );
+insert into tipo_producto_modelo  values ( 3 , 11 );
+insert into tipo_producto_modelo  values ( 3 , 12 );
+
+insert into gastos values (null, 50.5 , '2021-08-20' , 'tela datoka' );
+insert into gastos values (null, 50.5 , '2021-08-20' , 'tela acolchado' );
+insert into gastos values (null, 50.5 , '2021-08-20' , 'tela win' );
+insert into gastos values (null, 50.5 , '2021-08-20' , 'elastico' );
+
+
+insert into producto values(null, true ,'2021-05-07', 55.5,30, 1, 1, 1 , 1) ;
+insert into producto values(null, true ,'2021-05-07', 55.5,30, 2, 1, 1 , 1) ;
+insert into producto values(null, true ,'2021-05-07', 55.5,30, 3, 1, 1 , 1) ;
+insert into producto values(null, true ,'2021-05-07', 55.5,30, 2, 1, 2 , 1) ;
+insert into producto values(null, true ,'2021-05-07', 55.5,30, 1, 1, 3 , 1) ;
+
+insert into producto values(null, true ,'2021-05-07', 55.5,15, 3, 2, 1 , 1) ;
+insert into producto values(null, true ,'2021-05-07', 55.5,15, 2, 2, 1 , 1) ;
+insert into producto values(null, true ,'2021-05-07', 55.5,15, 4, 2, 1 , 1) ;
+insert into producto values(null, true ,'2021-05-07', 55.5,15, 5, 2, 2 , 1) ;
+insert into producto values(null, true ,'2021-05-07', 55.5,15, 6, 2, 3 , 1) ;
